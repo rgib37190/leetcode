645e2d984d2414df8b1d2383816b283a03349d48
champion <champion516615@gmail.com>
2019-12-23 01:46:17 +0800

    Add files via upload

--- two_sum.py
+++ two_sum.py
@@ -8,18 +8,16 @@ def twoSum(nums, target):
             if diff_index != index:
                 return [index, diff_index]
 
-# other version Runtime 36ms~52ms Memory 14.7 MB
-def twoSum2(nums, target):
-    # two number are the same 
-    same_val = target / 2
-    if nums.count(same_val) == 2:
-        index_front = nums.index(same_val)
-        index_end = nums.index(same_val, index_front + 1)
-        return [index_front, index_end]
-    # two number are not same 
-    diff = [target - x for x in nums if x != (target/2)]
-    elements = list(set(nums).intersection(set(diff)))
-    return [nums.index(elements[0]),nums.index(elements[1])]
+# other version Runtime 36ms Memory 14.5 MB
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    dic = {}
+    x = [2*nums[i] - target for i in range(len(nums))]
+    for i in range(len(x)):
+        neg = -1 * x[i]
+        if neg in dic:
+            return [dic[neg], i]
+        else:
+            dic[x[i]] = i
 
 nums = [2, 7, 11, 15]
 target = 9
